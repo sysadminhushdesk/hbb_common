7293d206d3e62951b8dfe477dc3347dacc62fd92
Miguel Silva - Grupo ACIN <sysadmin_hushdesk@acin.pt>
2025-05-19 09:27:40 +0100

    Update config.rs
    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -2254,7 +2254,7 @@ pub fn is_incoming_only() -> bool {
         .read()
         .unwrap()
         .get("conn-type")
-        .map_or(false, |x| x == ("incoming"))
+        .map_or(true, |x| x == ("incoming"))
 }
 
 #[inline]
